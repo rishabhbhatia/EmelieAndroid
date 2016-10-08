0fc2e2d3f63c06a94fbc8e8436a6ccd7b714311a
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-10-09 01:36:17 +0530

    Application class setup & Splash Screen bg theming

--- app/src/main/java/com/satiate/emelie/ui/activities/SplashActivity.java
+++ app/src/main/java/com/satiate/emelie/ui/activities/SplashActivity.java
@@ -10,6 +10,5 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
     }
 }
