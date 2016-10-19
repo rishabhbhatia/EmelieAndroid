920c8096a6e71b625af7db692ec46e2cedb875dd
Rishabh bhatia <rishabh.bhatia08@gmail.com>
2016-10-20 01:23:13 +0530

    Home Screen Nav icon setup

--- app/src/main/java/com/satiate/emelie/cardslidepanel/HomeCardFragment.java
+++ app/src/main/java/com/satiate/emelie/cardslidepanel/HomeCardFragment.java
@@ -1,8 +1,5 @@
 package com.satiate.emelie.cardslidepanel;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,15 +7,40 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.satiate.emelie.R;
 import com.satiate.emelie.models.Story;
 import com.satiate.emelie.utils.Const;
 import com.satiate.emelie.utils.EmelieUtilities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 @SuppressLint({"HandlerLeak", "NewApi", "InflateParams"})
 public class HomeCardFragment extends Fragment {
 
+    @BindView(R.id.iv_home_nav)
+    ImageView ivHomeNav;
+    @BindView(R.id.iv_home_add_story)
+    ImageView ivHomeAddStory;
+    @BindView(R.id.rl_card_header)
+    RelativeLayout rlCardHeader;
+    @BindView(R.id.card_left_btn)
+    Button cardLeftBtn;
+    @BindView(R.id.card_right_btn)
+    Button cardRightBtn;
+    @BindView(R.id.card_bottom_layout)
+    LinearLayout cardBottomLayout;
+    @BindView(R.id.image_slide_panel)
+    CardSlidePanel imageSlidePanel;
     private CardSlidePanel.CardSwitchListener cardSwitchListener;
 
     private List<Story> stories = new ArrayList<Story>();
@@ -28,6 +50,7 @@ public class HomeCardFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.card_layout, null);
         initView(rootView);
+        ButterKnife.bind(this, rootView);
         return rootView;
     }
 
@@ -61,10 +84,8 @@ public class HomeCardFragment extends Fragment {
 
         //create next 3 cards, Total 4, 1 vis & 3 behind it
         //create 100 cards
-        for (int j = 0; j < 3; j++)
-        {
-            for (int i = 0; i < 100; i++)
-            {
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 100; i++) {
                 Story story = new Story();
                 story.setTitle(EmelieUtilities.generateRandomStrings(5));
                 story.setLikesCount((int) (Math.random() * 10));
@@ -74,4 +95,14 @@ public class HomeCardFragment extends Fragment {
         }
     }
 
+    @OnClick({R.id.ll_home_nav, R.id.iv_home_add_story})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ll_home_nav:
+                Log.d(Const.TAG, "hello baby");
+                break;
+            case R.id.iv_home_add_story:
+                break;
+        }
+    }
 }
