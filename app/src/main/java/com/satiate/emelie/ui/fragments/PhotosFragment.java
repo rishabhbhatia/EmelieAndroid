84fa02bcc00dd88c43f2bf17ed0cea3a90d19d06
Rishabh bhatia <rishabh.bhatia08@gmail.com>
2016-10-20 21:27:22 +0530

    New Photos Fragment

---
+++ app/src/main/java/com/satiate/emelie/ui/fragments/PhotosFragment.java
@@ -0,0 +1,34 @@
+package com.satiate.emelie.ui.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.satiate.emelie.base.EmelieFragment;
+
+/**
+ * Created by Rishabh Bhatia on 20/10/16.
+ */
+
+public class PhotosFragment extends EmelieFragment {
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
